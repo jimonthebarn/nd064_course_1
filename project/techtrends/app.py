fbe857b3cc84a98dc2e0d98eac9504b35912c54e
Benjamin Roth <roth.benjamin@gmail.com>
2022-06-03 10:36:25 +0200

    Add application logs

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,7 +1,11 @@
 import sqlite3
+import logging
+import sys
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+
+connectionCount = 0
 
 
 # Function to get a database connection.
@@ -9,6 +13,8 @@ from werkzeug.exceptions import abort
 def get_db_connection():
     connection = sqlite3.connect('./database.db')
     connection.row_factory = sqlite3.Row
+    global connectionCount
+    connectionCount += 1
     return connection
 
 
@@ -41,14 +47,17 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+        app.logger.debug('Article not found!')
         return render_template('404.html'), 404
     else:
+        app.logger.debug('Article "%s" retrieved!' % post['title'])
         return render_template('post.html', post=post)
 
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info('Rendering about us page!')
     return render_template('about.html')
 
 
@@ -62,6 +71,7 @@ def create():
         if not title:
             flash('Title is required!')
         else:
+            app.logger.debug('Creating new article: %s!', title)
             connection = get_db_connection()
             connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
                                (title, content))
@@ -73,6 +83,43 @@ def create():
     return render_template('create.html')
 
 
+@app.route('/healthz')
+def healthz():
+    app.logger.debug('Retrieving application status')
+    response = app.response_class(
+        response=json.dumps({"result": "OK - healthy"}),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+@app.route('/metrics')
+def metrics():
+    app.logger.info('Retrieving application metrics')
+
+    posts = get_posts()
+    # im assuming we count number of initiated connections instead of total number of concurrent active connections
+    global connectionCount
+
+    response = app.response_class(
+        response=json.dumps({"db_connection_count": connectionCount, "post_count": len(posts)}),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+def get_posts():
+    connection = get_db_connection()
+    posts = connection.execute('SELECT * FROM posts').fetchall()
+    connection.close()
+    return posts
+
+
 # start the application on port 3111
 if __name__ == "__main__":
+    logging.basicConfig(stream=sys.stdout,
+                        level=logging.DEBUG,
+                        format='%(levelname)s::%(name)s::%(asctime)s, %(message)s')
     app.run(host='0.0.0.0', port='3111')
